5a3201dcb6d55a26cac3e9445fcc21a3e6d886f0
Manuel Reich <errnesto@me.com>
2017-01-08 11:37:27 +0100

    dont use bold or italic text for syntax higlighting it will break the editor sic

--- src/react_components/_helpers/markdownEditor.jsx
+++ src/react_components/_helpers/markdownEditor.jsx
@@ -1,39 +1,68 @@
 import React from 'react'
 import SyntaxHighlighter, { registerLanguage } from 'react-syntax-highlighter/dist/light'
 import markdown from 'react-syntax-highlighter/node_modules/highlight.js/lib/languages/markdown'
-import highlightStyle from 'react-syntax-highlighter/dist/styles/ascetic'
+import highlightStyle from './syntaxStyles'
 
 registerLanguage('markdown', markdown)
 
 export default class ComponentDemo extends React.Component {
-
-  shouldComponentUpdate () {
-    return false
+  constructor (props) {
+    super(props)
+    this.state = {
+      initialMarkdown: props.markdown,
+      markdown: props.markdown
+    }
   }
 
   render () {
-    const { onChange, markdown } = this.props
+    const { onChange } = this.props
+    const { initialMarkdown, markdown } = this.state
+    const wrapStyle = {
+      position: 'relative',
+      border: '1px solid #eee',
+      backgroundColor: 'white',
+      borderRadius: '2px',
+      margin: '-6px -9px'
+    }
+    const preStyle = {
+      position: 'absolute',
+      width: '100%',
+      whiteSpace: 'pre-wrap',
+      wordWrap: 'break-word',
+      backgroundColor: 'transparent',
+      margin: 0,
+      padding: '5px 8px'
+    }
+    const codeStyle = {
+      fontFamily: '"Noto Serif", serif',
+      lineHeight: '1.7em',
+      fontWeight: 'normal',
+      fontStyle: 'normal'
+    }
 
     return (
-      <div contentEditable
-        onInput={e => { onChange(e.target.innerText) }}
-        onBlur={e => { onChange(e.target.innerText) }}
-      >
+      <div style={wrapStyle}>
+        <pre style={preStyle}
+          contentEditable
+          onInput={e => {
+            this.setState({ markdown: e.target.innerText })
+            onChange(e.target.innerText)
+          }}
+        >
+          { /* initialMarkdown does not change with new props
+            so react won't update this component and the cursor will stay in tact */ }
+          <code style={codeStyle}>{ initialMarkdown }</code>
+        </pre>
         <SyntaxHighlighter
           language='markdown'
           style={highlightStyle}
           customStyle={{
-            whiteSpace: 'pre-wrap',
-            wordWrap: 'break-word',
-            border: '1px solid #eee',
-            borderRadius: '2px',
-            padding: '5px 8px',
-            margin: '-6px -9px'
+            ...preStyle,
+            position: 'relative',
+            zIndex: 10,
+            pointerEvents: 'none'
           }}
-          codeTagProps={{ style: {
-            fontFamily: '"Noto Serif", serif',
-            lineHeight: '1.7em'
-          } }}
+          codeTagProps={{ style: codeStyle }}
         >
           { markdown }
         </SyntaxHighlighter>
---
+++ src/react_components/_helpers/syntaxStyles.js
@@ -0,0 +1,36 @@
+'use strict'
+
+Object.defineProperty(exports, '__esModule', {
+  value: true
+})
+exports.default = {
+  'hljs': {
+    'display': 'block',
+    'background': 'white',
+    'color': '#222;'
+  },
+  'hljs-string': {
+    'color': '#538389'
+  },
+  'hljs-symbol': {
+    'color': '#c50061'
+  },
+  'hljs-bullet': {
+    'color': '#c50061'
+  },
+  'hljs-section': {
+    'color': '#9f223c'
+  },
+  'hljs-link': {
+    'color': '#538389'
+  },
+  'hljs-quote': {
+    'color': '#f5f5f5'
+  },
+  'hljs-strong': {
+    'color': '#005025'
+  },
+  'hljs-emphasis': {
+    color: '#d58f34'
+  }
+}
