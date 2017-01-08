8f4b683d713daf2f930556aaaf0fbaab7df55be7
Manuel Reich <errnesto@me.com>
2017-01-08 23:12:50 +0100

    move markdown display into markdown editor

--- src/react_components/_helpers/markdownEditor.jsx
+++ src/react_components/_helpers/markdownEditor.jsx
@@ -1,9 +1,25 @@
 import React from 'react'
 import SyntaxHighlighter, { registerLanguage } from 'react-syntax-highlighter/dist/light'
 import markdown from 'highlight.js/lib/languages/markdown'
+import marked from 'marked'
 import highlightStyle from './syntaxStyles'
 
 registerLanguage('markdown', markdown)
+const renderer = new marked.Renderer()
+// override markdown link render method
+// to add target="_blank" to links
+renderer.link = function (href, title, text) {
+  if (this.options.sanitize) {
+    try {
+      var prot = decodeURIComponent(unescape(href)).replace(/[^\w:]/g, '').toLowerCase()
+    } catch (e) { return '' }
+    if (prot.indexOf('javascript:') === 0 || prot.indexOf('vbscript:') === 0) return ''
+  }
+  var out = '<a href="' + href + '" target="_blank"'
+  if (title) out += ' title="' + title + '"'
+  out += '>' + text + '</a>'
+  return out
+}
 
 export default class ComponentDemo extends React.Component {
   constructor (props) {
@@ -15,11 +31,20 @@ export default class ComponentDemo extends React.Component {
   }
 
   render () {
-    const { onChange } = this.props
+    const { editMode, onChange, className } = this.props
     const { initialMarkdown, markdown } = this.state
 
+    if (!editMode) {
+      return (
+        <div id='markdown-editor' className={className} dangerouslySetInnerHTML={{ __html: marked(markdown, {
+          renderer: renderer,
+          sanitize: true
+        })}} />
+      )
+    }
+
     return (
-      <div id='markdown-editor'>
+      <div id='markdown-editor' className='editMode'>
         <pre className='editor'
           contentEditable
           onInput={e => {
--- src/react_components/stories/story.jsx
+++ src/react_components/stories/story.jsx
@@ -2,24 +2,7 @@
 
 import React, { PropTypes } from 'react'
 import { Link } from 'react-router'
-import marked from 'marked'
 import MarkdownEditor from '../_helpers/markdownEditor'
-
-const renderer = new marked.Renderer()
-// override markdown link render method
-// to add target="_blank" to links
-renderer.link = function (href, title, text) {
-  if (this.options.sanitize) {
-    try {
-      var prot = decodeURIComponent(unescape(href)).replace(/[^\w:]/g, '').toLowerCase()
-    } catch (e) { return '' }
-    if (prot.indexOf('javascript:') === 0 || prot.indexOf('vbscript:') === 0) return ''
-  }
-  var out = '<a href="' + href + '" target="_blank"'
-  if (title) out += ' title="' + title + '"'
-  out += '>' + text + '</a>'
-  return out
-}
 
 const Story = ({ parentPathname, slug, storyPage, pageNumber, isFirstPage, isLastPage, editMode }, { store }) => (
   <div id='story'>
@@ -29,15 +12,9 @@ const Story = ({ parentPathname, slug, storyPage, pageNumber, isFirstPage, isLas
       backgroundImage: `url(${storyPage.image})`,
       ...storyPage.imageStyles
     }} />
-    { editMode
-      ? <MarkdownEditor markdown={storyPage.markdown} onChange={markdown => {
-        store.setStoryPageMarkdown({ slug, pageNumber, markdown })
-      }} />
-      : <div className='story-text' dangerouslySetInnerHTML={{ __html: marked(storyPage.markdown, {
-        renderer: renderer,
-        sanitize: true
-      })}} />
-    }
+    <MarkdownEditor className='story-text' editMode={editMode} markdown={storyPage.markdown} onChange={markdown => {
+      store.setStoryPageMarkdown({ slug, pageNumber, markdown })
+    }} />
 
     { !isFirstPage && <Link className='prev arrow' to={`${pageNumber - 1}`}>←</Link> }
     { !isLastPage && <Link className='next arrow' to={`${pageNumber + 1}`}>→</Link> }
