2ed95f16b5b3d835f1761c207c23ef88dff76438
Manuel Reich <errnesto@me.com>
2017-01-16 20:09:40 +0100

    move stories edit panel over story image

--- src/react_components/stories/story.jsx
+++ src/react_components/stories/story.jsx
@@ -17,53 +17,55 @@ const Story = ({ parentPathname, slug, storyPage, pageNumber, numberOfPages, edi
         <h2 className='story-title'>{ storyPage.title }</h2>
       </Editable>
       <Link className={`close-button ${editMode && 'editMode'}`} to={parentPathname}>✕</Link>
-      <span className='story-image' style={{
+      <div className='story-image' style={{
         backgroundImage: `url(${storyPage.image})`,
         ...storyPage.imageStyles
-      }} />
+      }}>
+        { editMode &&
+          <div className='edit-panel'>
+            <button className='delete-page-button' onClick={() => {
+              store.stories.deletePage({ slug, pageNumber })
+            }}>Seite löschen</button>
+            <button className='create-page-button' onClick={() => {
+              const newPageNumber = pageNumber + 1
+              store.stories.createPage({ slug, newPageNumber })
+              router.transitionTo(`${newPageNumber + 1}`)
+            }}>Neue Seite erstellen</button>
+            <button className='edit-image-button'>Bild auswählen</button>
+          </div>
+        }
+      </div>
       <MarkdownEditor className='story-text' editMode={editMode} markdown={storyPage.markdown} onChange={markdown => {
         store.stories.setPageMarkdown({ slug, pageNumber, markdown })
       }} />
 
+      { (editMode && numberOfPages > 1) &&
+        <div className='page-number-panel'>
+          Seite:
+          <button className='page-number-button'
+            disabled={isFirstPage}
+            onClick={() => {
+              const newPageNumber = pageNumber - 1
+              store.stories.setPageNumber({ slug, pageNumber, newPageNumber })
+              router.transitionTo(`${newPageNumber + 1}`)
+            }}
+          >-</button>
+          <span className='page-number'>{ pageNumber + 1 }</span> / { numberOfPages }
+          <button className='page-number-button'
+            disabled={isLastPage}
+            onClick={() => {
+              const newPageNumber = pageNumber + 1
+              store.stories.setPageNumber({ slug, pageNumber, newPageNumber })
+              router.transitionTo(`${newPageNumber + 1}`)
+            }}
+          >+</button>
+        </div>
+      }
       { !isFirstPage &&
         <Link className='prev arrow' to={`${pageNumber}`}>←</Link>
       }
       { !isLastPage &&
         <Link className='next arrow' to={`${pageNumber + 2}`}>→</Link>
-      }
-      { editMode &&
-        <div className='edit-panel'>
-          <button className='delete-page-button' onClick={() => {
-            store.stories.deletePage({ slug, pageNumber })
-          }}>Seite löschen</button>
-          <button className='create-page-button' onClick={() => {
-            const newPageNumber = pageNumber + 1
-            store.stories.createPage({ slug, newPageNumber })
-            router.transitionTo(`${newPageNumber + 1}`)
-          }}>Neue Seite erstellen</button>
-          { numberOfPages > 1 &&
-            <p className='page-number-panel'>
-              Seite:
-              <button className='page-number-button'
-                disabled={isFirstPage}
-                onClick={() => {
-                  const newPageNumber = pageNumber - 1
-                  store.stories.setPageNumber({ slug, pageNumber, newPageNumber })
-                  router.transitionTo(`${newPageNumber + 1}`)
-                }}
-              >-</button>
-              <span className='page-number'>{ pageNumber + 1 }</span> / { numberOfPages }
-              <button className='page-number-button'
-                disabled={isLastPage}
-                onClick={() => {
-                  const newPageNumber = pageNumber + 1
-                  store.stories.setPageNumber({ slug, pageNumber, newPageNumber })
-                  router.transitionTo(`${newPageNumber + 1}`)
-                }}
-              >+</button>
-            </p>
-          }
-        </div>
       }
     </div>
   )
