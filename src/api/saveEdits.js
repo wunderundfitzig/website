2abe65a39c2b0a3c2e0cbd275cdf69573275d473
Manuel Reich <errnesto@me.com>
2017-01-25 21:25:03 +0100

    save stories and creatives !!without images

--- src/api/saveEdits.js
+++ src/api/saveEdits.js
@@ -5,23 +5,50 @@ import fs from 'fs'
 import path from 'path'
 import cache from './_cache'
 
+const DATA_PATH = path.join(__dirname, '../../data')
+
+/**
+ * encodes object as json string and writes it to file
+ * @param  {string} filename
+ * @param  {object} json
+ * @return {Promise} empty promise
+ */
+const writeJsonFile = ({ filename, json }) => {
+  return new Promise((resolve, reject) => {
+    fs.writeFile(filename, JSON.stringify(json), 'utf8', err => {
+      if (err) {
+        reject(err)
+      } else {
+        resolve()
+      }
+    })
+  })
+}
+
 export default (req, res) => {
-  const { creatives } = req.body
+  const { creatives, stories } = req.body
+  const writeActions = []
 
   if (creatives && creatives.length !== 0) {
-    fs.writeFile(
-      path.join(__dirname, '../../data/creatives.json'),
-      JSON.stringify(creatives),
-      'utf8',
-      err => {
-        if (err) {
-          console.log(err)
-          res.sendStatus(HTTPStatus.INTERNAL_SERVER_ERROR)
-        } else {
-          res.sendStatus(HTTPStatus.OK)
-          cache.invalidate('/creatives')
-        }
-      }
-    )
+    writeActions.push(writeJsonFile({
+      filename: path.join(DATA_PATH, 'creatives.json'),
+      json: creatives
+    }))
   }
+  if (stories && stories.length !== 0) {
+    writeActions.push(writeJsonFile({
+      filename: path.join(DATA_PATH, 'stories.json'),
+      json: stories
+    }))
+  }
+
+  Promise.all(writeActions)
+  .then(() => {
+    cache.invalidate('/creatives')
+    res.sendStatus(HTTPStatus.OK)
+  })
+  .catch(err => {
+    console.log(err)
+    res.sendStatus(HTTPStatus.INTERNAL_SERVER_ERROR)
+  })
 }
