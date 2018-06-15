67d733bf0ee307c2d1976c2c0e6da2d32923b106
Manuel Reich <mr@wundf.net>
2018-06-15 16:12:47 +0200

    Store images from fb on server^:wq

--- src/api/newsFeed.js
+++ src/api/newsFeed.js
@@ -1,6 +1,9 @@
 'use strict'
 
 import fetch from 'node-fetch'
+import request from 'request'
+import fs from 'fs'
+import path from 'path'
 import cache from './_cache'
 
 export default (req, res) => {
@@ -9,6 +12,10 @@ export default (req, res) => {
 
   fetch('https://graph.facebook.com/wunderundfitzig/feed?' +
         `fields=${fields}&access_token=${accessToken}&limit=10`)
+  .then(res => {
+    console.log(res)
+    return res
+  })
   .then(fbRes => fbRes.json())
   .then(feed => feed.data.filter(post =>
       (post.type === 'photo' || post.type === 'link') && post.object_id))
@@ -17,12 +24,30 @@ export default (req, res) => {
           `fields=images&access_token=${accessToken}`)
     .then(res => res.json())
     .then(res => {
-      post.picture = res.images[0] // images[0] should be the largest one
+      const image = res.images[0] // images[0] should be the largest one
+      const basename = path.basename(image.source)
+      const imgName = basename.split('?')[0]
+      const filename = path.join(process.env.IMAGE_PATH, 'newsFeed', imgName)
+
+      download(image.source, filename)
+      post.picture = {
+        ...image,
+        source: `/assets/imgs/newsFeed/${imgName}`
+      }
       return post
     })
   ))))
   .then(feed => {
     if (!res.headersSent) res.send(feed)
     cache.store({ key: req.path, json: feed })
+  })
+}
+
+// from https://stackoverflow.com/questions/12740659/downloading-images-with-node-js#12751657
+
+var download = function(uri, filename){
+  request.head(uri, function(err, res, body) {
+    if (err) return
+    request(uri).pipe(fs.createWriteStream(filename))
   })
 }
